c7c399177a628aa1fefb597e681efa827bacbd80
Nic <nic@nicsknickknackshack.com>
2018-09-22 10:55:45 -0400

    Setup project settings to include Paper2D

---
+++ Source/MasterBlaster/BaseUnit.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "BaseUnit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASTERBLASTER_API ABaseUnit : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
