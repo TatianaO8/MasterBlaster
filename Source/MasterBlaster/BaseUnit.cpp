bc7c496c90acb3cece251729e8b3493099c8e15d
Travis Kerr <kerrta@users.noreply.github.com>
2018-09-23 03:24:54 -0400

    Implemented health in BaseUnit

--- Source/MasterBlaster/BaseUnit.cpp
+++ Source/MasterBlaster/BaseUnit.cpp
@@ -1,7 +1,71 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BaseUnit.h"
+#include "Kismet/KismetMathLibrary.h"
 
 
+// Sets default values
+ABaseUnit::ABaseUnit()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
+}
+
+// Called when the game starts or when spawned
+void ABaseUnit::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FullHealth = 100.0f;
+	Health = FullHealth;
+	HealthPercentage = 1.0f;
+	bCanBeDamaged = true;
+}
+
+// Called every frame
+void ABaseUnit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABaseUnit::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+float ABaseUnit::GetHealth()
+{
+	return HealthPercentage;
+}
+
+FText ABaseUnit::GetHealthIntText()
+{
+	int32 hp = FMath::RoundHalfFromZero(HealthPercentage * 100);
+	FString hps = FString::FromInt(hp);
+	FString healthHUD = hps + FString(TEXT("%"));
+	FText hpText = FText::FromString(healthHUD);
+	return hpText;
+}
+
+void ABaseUnit::SetDamageState()
+{
+	bCanBeDamaged = true;
+}
+
+float ABaseUnit::TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser)
+{
+	bCanBeDamaged = false;
+	UpdateHealth(-DamageAmount);
+	return DamageAmount;
+}
+
+void ABaseUnit::UpdateHealth(float HealthChange)
+{
+	Health = FMath::Clamp(Health += HealthChange, 0.0f, FullHealth);
+	HealthPercentage = Health / FullHealth;
+}
 
--- Source/MasterBlaster/BaseUnit.h
+++ Source/MasterBlaster/BaseUnit.h
@@ -11,9 +11,48 @@
  */
 UCLASS()
 class MASTERBLASTER_API ABaseUnit : public APaperCharacter{
+	
 	GENERATED_BODY()
-	
-	
-	
+public:
+	// Sets default values for this character's properties
+	ABaseUnit();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser);
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+		float FullHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+		float Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+		float HealthPercentage;
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+		float GetHealth();
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FText GetHealthIntText();
+
+	UFUNCTION()
+		void SetDamageState();
+
+
+	// void ReceivePointDamage(float Damage, const UDamageType * DamageType, FVector HitLocation, FVector HitNormal, UPrimitiveComponent * Hit Component, FName BoneName, FVector ShotFromDirection, AController * InstigatedBy, AActor * DamageCauser, const FHitResult & HitInfo);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void UpdateHealth(float HealthChange);
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 };
