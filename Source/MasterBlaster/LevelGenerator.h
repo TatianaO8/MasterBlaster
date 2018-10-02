19e575e768faa01b464d9127c4afceeaf6b8e345
MrLever <MrLeverx@gmail.com>
2018-10-02 19:07:14 -0400

    Remove old map generation code

--- Source/MasterBlaster/LevelGenerator.h
+++ Source/MasterBlaster/LevelGenerator.h
@@ -19,11 +19,6 @@ public:
 	ALevelGenerator();
 
 private:
-	void GetTileSpawnLocation(const ABaseTile* previousTile, FVector &location, bool &canChangeDirection);
-	void GetTileSpawnRotation(const ABaseTile* previousTile, FRotator &rotation, bool canChangeDirection);
-	void GenerateFloor();
-	void GenerateCeiling();
-	void GenerateMap();
 
 protected:
 	// Called when the game starts or when spawned
@@ -33,25 +28,10 @@ public:
 	TArray<ABaseTile*> floor;
 	TArray<ABaseTile*> ceiling;
 
-	UPROPERTY(EditAnywhere, Category = LevelGeneration)
-	int mapWidth;
-
-	UPROPERTY(EditAnywhere, Category = LevelGeneration)
-	int ceilingHeight;
-
-	UPROPERTY(EditAnywhere, Category = LevelGeneration)
-	int tileSize;
-	
-	UPROPERTY(EditAnywhere, Category = LevelGeneration)
-	int slope;
-	
-	UPROPERTY(EditAnywhere, Category = LevelGeneration)
-	TSubclassOf<ABaseTile> tile;
-
+	UPROPERTY(EditAnywhere, Category = Rooms)
+	TArray<ABaseTile*> test;
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-private:
-	const float pi = 3.141592;
 	
 };
